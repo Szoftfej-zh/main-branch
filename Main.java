7eede93a09bfa2fa1120bd2e12ff421cf3c88758
Asetitmagus <79262753+Asetitmagus@users.noreply.github.com>
2021-05-12 15:22:29 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -17,24 +18,30 @@ public class Main {
         
         //Az elérési útvonal változhat!
         URL url = new URL("http://localhost/database/send.php");
-        URLConnection conn = url.openConnection();
-        
-        conn.setDoOutput(true);
-        PrintStream send = new PrintStream(conn.getOutputStream());
-        
-        
+
+        ArrayList<URLConnection> conns = new ArrayList<URLConnection>();     
+        ArrayList<PrintStream> sends = new ArrayList<PrintStream>();
         
         for (int i = 0; i < console.getArray().size(); i++) {
-            send.print("&Sequence=" + console.getArray().get(i).getNumber());
-            send.print("&Name=" + console.getArray().get(i).nameOfCity);
-            send.print("&X=" + console.getArray().get(i).getX());
-            send.print("&Y=" + console.getArray().get(i).getY());
-            send.print("&random1=" + console.getArray().get(i).getasd());
-            send.print("&random2=" + console.getArray().get(i).getasd1());
-            
-            conn.getInputStream();
+            conns.add(url.openConnection());
+            conns.get(i).setDoOutput(true);
+            sends.add(new PrintStream(conns.get(i).getOutputStream()));
         }
         
-        send.close();
+        for (int i = 0; i < console.getArray().size(); i++) {
+            sends.get(i).print("&Sequence=" + console.getArray().get(i).getNumber());
+            sends.get(i).print("&Name=" + console.getArray().get(i).getCity());
+            sends.get(i).print("&X=" + console.getArray().get(i).getX());
+            sends.get(i).print("&Y=" + console.getArray().get(i).getY());
+            sends.get(i).print("&random1=" + console.getArray().get(i).getasd());
+            sends.get(i).print("&random2=" + console.getArray().get(i).getasd1());
+            
+            conns.get(i).getInputStream();           
+        }        
+        
+        for (int i = 0; i < console.getArray().size(); i++) {
+            sends.get(i).close();
+        }
+        System.out.println("Feltöltés befejezve");
     }    
 }
--- send.php
+++ send.php
@@ -20,22 +20,23 @@
 				$random2 = $value;
 				break;
 		}
-	
-		$conn = new mysqli("localhost", "root", "", "data");
+	}
+		$conn = new mysqli("localhost", "root", "", "urbanization");
 		
 		if ($conn->connect_error){
 			die("Connection failed: " + $conn->connect_error);
 		}
 	
 		$sql = "INSERT INTO data (SequenceNumber, CityName, X, Y, random1, random2) 
-				VALUES (Sequence, Name, X, Y, random1, random2)";
+				VALUES ({$_POST["Sequence"]}, '{$_POST["Name"]}', {$_POST["X"]}, 
+				{$_POST["Y"]}, {$_POST["random1"]}, {$_POST["random2"]})";
 		
 		if ($conn->query($sql) === TRUE) {
-			echo "Succesfuly sent!"
+			echo "Succesfuly sent!";
 		}else {
 			echo "Error: " . $sql . "<br>" . $conn->error;
 		}
-	}
+	
 
 	$conn->close();
 ?>
